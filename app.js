0460300471aa075d925e032f499ebe6f5345f44c
furaihan <28698437+furaihan@users.noreply.github.com>
2023-04-13 11:14:11 +0700

    reorder, work button

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 let display = document.getElementById('display');
 
 function addToDisplay(value) {
+    if (display.value === '0'){
+        display.value = '';
+    }
     display.value += value;
 }
 
 function clearDisplay() {
     display.value = '';
+}
+
+function backspace() {
+    display.value = display.value.slice(0, -1);
+}
+
+function negate(){
+    display.value = display.value * -1;
 }
 
 function calculate() {
