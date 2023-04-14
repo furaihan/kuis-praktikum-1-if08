1cf34980aee977981d25ddaddf00945a2156ad42
aric4381 <aricyohanes@amikom.ac.id>
2023-04-14 20:41:31 +0700

    stying calculator

--- app.js
+++ app.js
@@ -31,6 +31,12 @@ function toggleLightMode() {
     document.body.classList.remove("light");
     document.querySelector(".displayInput").classList.remove("light");
     document.querySelector(".calculator").classList.remove("light");
+    document.querySelector(".btnBackground").classList.remove("light");
+    document.querySelector(".toggleBtn").classList.remove("light");
+    document.querySelector(".toggleBtn .fa-sun").classList.remove("light");
+    document.querySelector(".toggleBtn .fa-moon").classList.remove("light");
+    document.querySelector(".jam").classList.remove("light");
+    document.querySelector(".utility").classList.remove("light");
     document.querySelectorAll(".numBtn").forEach((item) => {
       item.classList.remove("light");
     });
@@ -44,6 +50,12 @@ function toggleLightMode() {
     document.body.classList.add("light");
     document.querySelector(".displayInput").classList.add("light");
     document.querySelector(".calculator").classList.add("light");
+    document.querySelector(".btnBackground").classList.add("light");
+    document.querySelector(".toggleBtn").classList.add("light");
+    document.querySelector(".fa-sun").classList.add("light");
+    document.querySelector(".fa-moon").classList.add("light");
+    document.querySelector(".jam").classList.add("light");
+    document.querySelector(".utility").classList.add("light");
     document.querySelectorAll(".numBtn").forEach((item) => {
       item.classList.add("light");
     });
