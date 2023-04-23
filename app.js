f4a47a68b6228a2d4e49b3de110898fc61bca0bd
furaihan <28698437+furaihan@users.noreply.github.com>
2023-04-23 23:22:57 +0700

    tweak docs for app.js

--- app.js
+++ app.js
@@ -1,59 +1,59 @@
-let display = document.getElementById("display");
+/**
 
-/*
-
-Fungsi untuk menambahkan nilai ke dalam tampilan kalkulator.
-Jika nilai tampilan saat ini adalah "0", maka nilai tersebut akan dihapus sebelum menambahkan nilai baru.
-@param {string} value - Nilai yang akan ditambahkan ke tampilan.
+Adds a value to the calculator display. If the current display value is "0", it will be removed before the new value is added.
+@param {string} value - The value to be added to the display.
+@returns {void}
 */
 function addToDisplay(value) {
   if (display.value === "0") {
     display.value = "";
   }
-    display.value += value;
-  }
-  /*
-  
-  Fungsi untuk menghapus nilai di tampilan kalkulator.
-  */
-  function clearDisplay() {
-    display.value = "";
-  }
-  /*
-  
-  Fungsi untuk menghapus karakter terakhir di tampilan kalkulator.
-  */
-  function backspace() {
-    display.value = display.value.slice(0, -1);
-  }
-  /*
-  
-  Fungsi untuk mengubah nilai di tampilan kalkulator menjadi nilai yang berlawanan (negatif menjadi positif dan sebaliknya).
-  */
-  function negate() {
-    display.value = display.value * -1;
-  }
-  /*
-  
-  Fungsi untuk menghitung nilai yang ada di tampilan kalkulator menggunakan fungsi eval() bawaan JavaScript.
-  Jika terjadi kesalahan dalam perhitungan, maka akan ditampilkan pesan "Error" di tampilan.
-  */
-  function calculate() {
-    try {
-      display.value = eval(display.value);
-    } catch (error) {
-      display.value = "Error: " + error.message;
-    }
-  }
-  
+  display.value += value;
+}
 /**
- * toggleLightMode - A recursive function to toggle the "light" class on a list of elements.
- *
- * @param {number} index - The current index of the class to be processed in the classes array.
- *                        Defaults to 0.
- *
- * @returns {void}
- */  
+
+Clears the calculator display.
+@returns {void}
+*/
+function clearDisplay() {
+  display.value = "";
+}
+/**
+
+Removes the last character from the calculator display.
+@returns {void}
+*/
+function backspace() {
+  display.value = display.value.slice(0, -1);
+}
+/**
+
+Changes the sign of the calculator display value (positive to negative or vice versa).
+@returns {void}
+*/
+function negate() {
+  display.value = display.value * -1;
+}
+/**
+
+Calculates the value of the expression in the calculator display using the built-in JavaScript eval() function.
+If an error occurs during the calculation, an "Error" message will be displayed on the calculator display.
+@returns {void}
+*/
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error: " + error.message;
+  }
+}
+/**
+
+A recursive function that toggles the "light" class on a list of elements. The function processes the elements in the classes array one by one.
+When the end of the array is reached, the function stops.
+@param {number} index - The current index of the class to be processed in the classes array. Defaults to 0.
+@returns {void}
+*/
 function toggleLightMode(index = 0) {
   const classes = [
     ".body",
@@ -67,16 +67,15 @@ function toggleLightMode(index = 0) {
     ".utility",
     ".numBtn",
     ".cyanBtn",
-    ".opBtn"
+    ".opBtn",
   ];
-
   if (index === classes.length) {
     return; // Base case: exit when all classes have been processed
   }
 
   const elements = document.querySelectorAll(classes[index]);
 
-  elements.forEach(element => {
+  elements.forEach((element) => {
     if (element.classList.contains("light")) {
       element.classList.remove("light");
     } else {
@@ -86,5 +85,3 @@ function toggleLightMode(index = 0) {
 
   toggleLightMode(index + 1); // Recursive call to process next class
 }
-
-
